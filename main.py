488c22b59c5b0f5bd6a8a70131548b4653724012
Dave Sugden <davelms@gmail.com>
2022-01-23 17:43:30 -0500

    Update Hello World message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World Release 2!"
 # run the app.
 if __name__ == "__main__":
     app.run()
